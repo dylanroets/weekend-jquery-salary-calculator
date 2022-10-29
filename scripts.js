be166ce3d72c7c3c96e757576d33d07e8923f907
Dylan Roets <dylanroets@yahoo.com>
2022-10-29 15:07:20 -0500

    got the button to grab employee values, now I'll push those to an array

--- scripts.js
+++ scripts.js
@@ -8,9 +8,24 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('JQ working');
-    
+    //testing to see JQ connected
+
+    //adding submit employee button
+    $('#submitEmployeeBtn').on('click', addEmployee);
 
 
 
 }
-
+//adding function to get info from input and store into array of employees
+function addEmployee() {
+    let employee = {
+        firstName: $('#firstNameInput').val(),
+        lastName: $('#lastNameInput').val(),
+        id: $('#idInput').val(),
+        title: $('#titleInput').val(),
+        salary: $('#salaryInput').val()
+        
+    }
+    //testing if function works
+    console.log('employee values: ', employee );
+}
